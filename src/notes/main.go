d137f95b2348480bf931fd6cf0ffba7cf5a53092
samling <samlingx@gmail.com>
2017-04-07 00:29:37 -0700

    Working cursor and vim editor struct

--- src/notes/main.go
+++ src/notes/main.go
@@ -19,81 +19,96 @@ var (
 	currNote int
 )
 
+type VimEditor struct {
+	Insert bool
+}
+
+func (ve *VimEditor) Edit(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+	if ve.Insert {
+		ve.InsertMode(v, key, ch, mod)
+	} else {
+		ve.NormalMode(v, key, ch, mod)
+	}
+}
+
+func (ve *VimEditor) InsertMode(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+	switch {
+	case key == gocui.KeyEsc:
+		ve.Insert = false
+	case ch != 0 && mod == 0:
+		v.EditWrite(ch)
+	case key == gocui.KeySpace:
+		v.EditWrite(' ')
+	case key == gocui.KeyBackspace || key == gocui.KeyBackspace2:
+		v.EditDelete(true)
+	case key == gocui.KeyDelete:
+		v.EditDelete(false)
+	case key == gocui.KeyInsert:
+		v.Overwrite = !v.Overwrite
+	case key == gocui.KeyEnter:
+		v.EditNewLine()
+	case key == gocui.KeyArrowDown:
+		v.MoveCursor(0, 1, false)
+	case key == gocui.KeyArrowUp:
+		v.MoveCursor(0, -1, false)
+	case key == gocui.KeyArrowLeft:
+		v.MoveCursor(-1, 0, false)
+	case key == gocui.KeyArrowRight:
+		v.MoveCursor(1, 0, false)
+	}
+	// TODO: handle other keybindings...
+}
+
+func (ve *VimEditor) NormalMode(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+	switch {
+	case ch == 'i':
+		ve.Insert = true
+	case ch == 'j':
+		v.MoveCursor(0, 1, false)
+	case ch == 'k':
+		v.MoveCursor(0, -1, false)
+	case ch == 'h':
+		v.MoveCursor(-1, 0, false)
+	case ch == 'l':
+		v.MoveCursor(1, 0, false)
+	}
+	// TODO: handle other keybindings...
+}
+
 func check(e error) {
 	if e != nil {
 		panic(e)
 	}
 }
 
-func main() {
-	// Set the default current note
-	// TODO: Make this save the last open note
-	currNote = 1
-
-	// Open a database connection
-	db = InitDB(dbpath)
-	defer db.Close()
-
-	// Create our tables if they don't exist
-	CreateTables(db)
-
-	// Retrieve the list of notes
-	rows := GetNoteTitles(db)
-	row := GetNoteBody(db, currNote)
-
-	// Set up the GUI
-	gui, err := gocui.NewGui(gocui.OutputNormal)
-	check(err)
-	defer gui.Close()
-
-	// Set our layout for the GUI
-	gui.SetManagerFunc(layout)
-
-	// Enable the cursor
-	gui.Cursor = true
-
-	// List out all of our notes
-	DisplayNoteTitles(gui, rows)
-
-	// Display the body of our current note
-	DisplayNoteBody(gui, row)
-
-	// Ctrl-C keybinding
-	if err := keybindings(gui); err != nil {
-		log.Panicln(err)
-	}
-
-	// Check for loops in the main window
-	if err := gui.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
-	}
-
-	//n := &Note{"second title", "second value", "second tag"}
-	//testTag := &Tag{"test name", "test member"}
-	//n.Save(db)
-
-	//n.Load(db)
-	//fmt.Printf("%t\n", res)
-
-	//res = testTag.Add(db)
-	//fmt.Printf("%t\n", res)
-}
-
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
 
-	if _, err := g.SetView("sidebar", -1, -1, 30, maxY); err != nil {
+	if s, err := g.SetView("sidebar", -1, -1, 30, maxY); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-
+		s.Title = "Notes"
+		s.Editable = true
+		s.Wrap = true
+		s.Editor = &VimEditor{}
 	}
 
-	if _, err := g.SetView("main", 30, -1, maxX, maxY); err != nil {
+	if m, err := g.SetView("main", 30, -1, maxX, maxY); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
+		m.Title = "Body"
+		m.Editable = true
+		m.Wrap = true
+		m.Editor = &VimEditor{}
+
 	}
+
+	if _, err := g.SetCurrentView("sidebar"); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -139,4 +154,63 @@ func cursorUp(g *gocui.Gui, v *gocui.View) error {
 		}
 	}
 	return nil
+}
+
+func main() {
+	// Set the default current note
+	// TODO: Make this save the last open note
+	currNote = 1
+
+	// Open a database connection
+	db = InitDB(dbpath)
+	defer db.Close()
+
+	// Create our tables if they don't exist
+	CreateTables(db)
+
+	// Retrieve the list of notes
+	rows := GetNoteTitles(db)
+	row := GetNoteBody(db, currNote)
+
+	// Set up the GUI
+	gui, err := gocui.NewGui(gocui.OutputNormal)
+	check(err)
+	defer gui.Close()
+
+	// Set our layout for the GUI
+	gui.SetManagerFunc(layout)
+	gui.InputEsc = true
+
+	// Enable the cursor
+	gui.Cursor = true
+
+	// Set the foreground color
+	gui.FgColor = gocui.ColorGreen
+	gui.BgColor = gocui.ColorBlack
+
+	// List out all of our notes
+	DisplayNoteTitles(gui, rows)
+
+	// Display the body of our current note
+	DisplayNoteBody(gui, row)
+
+	// Ctrl-C keybinding
+	if err := keybindings(gui); err != nil {
+		log.Panicln(err)
+	}
+
+	// Check for loops in the main window
+	if err := gui.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+
+	//n := &Note{"second title", "second value", "second tag"}
+	//testTag := &Tag{"test name", "test member"}
+	//n.Save(db)
+
+	//n.Load(db)
+	//fmt.Printf("%t\n", res)
+
+	//res = testTag.Add(db)
+	//fmt.Printf("%t\n", res)
 }
